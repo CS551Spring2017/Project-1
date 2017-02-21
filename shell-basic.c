0f03ddec823b1bb39729b1647b5ab8dd585ac7df
Randal Kwok <randalkwok@yahoo.com>
2017-02-20 19:40:44 -0600

    Reading profile

--- shell-basic.c
+++ shell-basic.c
@@ -11,6 +11,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_ARGS 100
+#define MAX_COMMAND_LENGTH 100
+
 static char *prompt = "shell> ";
 static char done = 0;
 
@@ -19,6 +25,10 @@ int shell_process(char *arg);
 int verify_parenthesis_count(char *cmd);
 int shell_sequence(char *cmd);
 int shell_parallel(char *cmd);
+int parseCmd(char *cmdline, char **argv);
+void loadProfile();
+
+char path[100];
 
 int main()
 {
@@ -55,7 +65,7 @@ int parseline()
 		if (c == '\r' || c == '\n') // 'enter' pressed
 		{
 			ready = 1;
-			buffer[i++] = '\0';
+			buffer[i++] = ' ';
 		}
 		else if (c == '\t') // 'tab' pressed
 		{
@@ -77,7 +87,7 @@ int parseline()
 			}
 		}
 		else {
-			printf("%c", c);
+			printf("\b%c", c);
 			buffer[i++] = c;
 		}
 	}
@@ -85,8 +95,10 @@ int parseline()
 	
 	size_t len = strlen(buffer);
 	printf(" [I='%s'] [%d chars]\n", buffer, len);
-	
+	loadProfile();
 	shell_process(buffer);
+	//buffer[i] = ' ';buffer[i+1] = '\0';
+	shell_sequence(buffer);
 	
 	return 1;
 }
@@ -144,9 +156,111 @@ int verify_parenthesis_count(char *cmd)
 	return 0;
 }
 
+int parseCmd(char *command, char **argv){
+	char *p2 = strtok(command, " ");
+	int i = 0;
+
+	argv[i++] = p2;
+	//printf("Cmd: %s\n", p2);
+
+	while ((p2 = strtok(NULL, " ")) != NULL)
+	{
+		if (i < MAX_ARGS)
+		{
+			argv[i++] = p2;
+			//printf("Arg(s): %s\n", p2);
+		}
+		else
+		{
+			perror("Too many arguments.\n");
+		}
+	}
+	argv[i] = NULL;
+}
 int shell_sequence(char *cmd)
 {
+	/**
 	printf("You entered the sequence function\n");
+	char *argv[MAX_ARGS];
+	printf("cmd: %s\n",cmd);
+	int argc = parseCmd(cmd, argv);
+	printf("argv[0]: %s\n", argv[0]);
+	
+	int status;
+	pid_t pid1 = fork();
+	if(pid1 == 0)
+	{
+		execv(argv[0],argv);
+	}
+	else{
+		waitpid(pid1, &status,0);
+	}
+	**/
+
+	char *cmd1, *cmd2;
+	int i1 = 0;
+	int i2 = 0;
+	int foundSemi = 0;
+	
+	cmd1 = (char *)malloc(32 * sizeof(char));
+	cmd2 = (char *)malloc(32 * sizeof(char));
+	int i;
+	for(i = 0; i <= strlen(cmd); i++){
+		if(cmd[i] == ';')
+		{
+			foundSemi = 1;
+			continue;
+		}
+		if(!foundSemi)
+		{
+			cmd1[i1] = cmd[i];
+			i1++;
+		}
+		else
+		{
+			cmd2[i2] = cmd[i];
+			i2++;
+		}
+	}
+	if(!foundSemi)
+	{
+		return 0;
+	}
+	cmd1[i1] = ' ';
+	cmd2[i2] = ' ';
+
+	char *argv1[MAX_ARGS];
+	parseCmd(cmd1, argv1);
+	char *argv2[MAX_ARGS];
+	parseCmd(cmd2, argv2);	
+	printf("cmd1: %s\n", cmd1);
+	printf("cmd2: %s\n", cmd2);
+	printf("argv1[0]: %s\n", argv1[0]);
+	printf("argv2[0]: %s\n", argv2[0]);
+	
+	int status;
+	pid_t pid1 = fork();
+	if(pid1 == 0)
+	{
+		char *testarg[] = {"/bin/ls",NULL};
+		execvp(argv1[0],argv1);
+		printf("%s: Command not found\n", argv1[0]);
+		exit(0);
+	}
+	else{
+		waitpid(pid1, &status,0);
+	}
+
+	pid_t pid2 = fork();
+	if(pid2 == 0)
+	{
+		execvp(argv2[0],argv2);
+		printf("%s: Command not found\n", argv2[0]);
+		exit(0);
+	}
+	else{
+		waitpid(pid2, &status,0);
+	}
 	return 1;
 }
 
@@ -154,4 +268,23 @@ int shell_parallel(char *cmd)
 {
 	
 	return 1;
+}
+
+void loadProfile()
+{
+	FILE *profile;
+	char *pathread;
+	size_t bufsize = 100;
+	size_t characters;
+	profile = fopen("PROFILE", "r");
+	if (profile == NULL)
+	{
+        perror("PROFILE not found");
+		return;
+	}
+	characters = getline(&pathread, &bufsize, profile);
+	printf("PATH set to: %s", pathread);
+	putenv(pathread);
+	char home[] = "/usr/";
+	chdir(home);
 }
